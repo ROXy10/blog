f4108755ef989ad78fb287be62b1fc899516029e
ROXy <djeffik@gmail.com>
2017-01-17 00:19:30 +0200

    24 - User Login & Form Validation

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, get_user_model, login, logout
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .form import UserLoginForm
+from .forms import UserLoginForm, UserRegisterForm
 
 
 def login_view(request):
@@ -12,14 +12,32 @@ def login_view(request):
         password = form.cleaned_data.get('password')
         user = authenticate(username=username, password=password)
         login(request, user)
+        return redirect('/')
 
-    return render(request, 'form.html', {'form':form, 'title':title})
+    return render(request, 'form.html', {'form': form, 'title': title})
 
 
 def register_view(request):
-    return render(request, 'form.html', {})
+    title = 'Register'
+    form = UserRegisterForm(request.POST or None)
+
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        return redirect('/')
+
+    context = {
+        'title': title,
+        'form': form,
+    }
+    return render(request, 'form.html', context)
 
 
 def logout_view(request):
     logout(request)
-    return render(request, 'form.html', {})
+    return redirect('/')
--- src/blog/urls.py
+++ src/blog/urls.py
@@ -23,12 +23,13 @@ from accounts.views import login_view, register_view, logout_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/', include("posts.urls", namespace='posts')),
     url(r'^comments/', include("comments.urls", namespace='comments')),
+    url(r'^register/', register_view, name='register'),
     url(r'^login/', login_view, name='login'),
     url(r'^logout/', logout_view, name='logout'),
+    url(r'^', include("posts.urls", namespace='posts')),
 
-    #url(r'^posts/$', "<appname>.views.<function_name>"),
+    # url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
 
 if settings.DEBUG:
